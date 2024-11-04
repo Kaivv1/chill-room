cda4b16b30438b70924380260bc2316de76ff931
Kaivv1 <kvalchevv1@gmail.com>
2024-11-04 09:39:21 +0200

    jwt

--- server/api/jwt.go
+++ server/api/jwt.go
@@ -6,12 +6,21 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func createJwtToken(val, secret string, duration time.Duration) {
+func CreateJwtToken(val, secret string, duration time.Duration) (string, error) {
 	claims := jwt.RegisteredClaims{
 		Issuer:    "chill-room",
 		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
 		ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
 		Subject:   val,
 	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, err
+}
+
+func VerifyJwtToken(token string) {
 
 }
